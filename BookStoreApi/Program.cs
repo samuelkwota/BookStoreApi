f55dfcbf80f069dc32542f2120e8546c04526158
Samuel Kwota <Sfuturemygamesworld@outlook.com>
2025-03-21 12:57:28 -0800

    Add project files.

---
+++ BookStoreApi/Program.cs
@@ -0,0 +1,31 @@
+using BookStoreApi.Models;
+using BookStoreApi.Services;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+
+builder.Services.AddSingleton<BooksService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
